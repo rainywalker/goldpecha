e587cb2d8585fe21f8f3c86906f5d957289fd923
jaelomin <rainywalker@gmail.com>
2018-03-03 02:22:29 +0900

    delete list logic updated

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,10 +1,54 @@
 <template>
-
+    <div>
+        <ul>
+            <li v-for="user in users">
+                <span>{{user.username}}</span>
+                <span>{{user.phone}}</span>
+                <span>{{user.car}}</span>
+                <button type="button" @click="removeUser(user._id)">삭제</button>
+            </li>
+        </ul>
+        <ul v-if="errors && errors.length">
+            <li v-for="error of errors">
+                {{error.message}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "UserList"
+        name: "UserList",
+        created () {
+            this.initList()
+        },
+
+        data() {
+            return {
+                users : [],
+                errors : []
+            }
+        },
+        methods: {
+            initList() {
+                this.$http.get(`/api/user`)
+                .then(response => {
+                    this.users = response.data;
+                })
+                .catch(e => {
+                    this.errors.push(e)
+                })
+            },
+            removeUser (userId) {
+                this.$http.delete(`/api/user/${userId}`)
+                .then(res => {
+                    this.initList()
+                })
+                .catch(e => {
+                    this.errors.push(e)
+                })
+            }
+        }
     }
 </script>
 
