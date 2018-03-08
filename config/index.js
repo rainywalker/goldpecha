a092d73511f8693e822c6fc92f7696b3da718464
jaelomin <jaelomin@jaelominui-MacBook-Pro.local>
2018-03-09 01:50:29 +0900

    webpack devtoll chaged

--- config/index.js
+++ config/index.js
@@ -60,7 +60,8 @@ module.exports = {
 
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: 'nosources-source-map',
+    devtool: '#nosources-source-map',
+      // cheap-module-source-map
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
