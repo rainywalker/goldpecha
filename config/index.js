104a40b80bcb3f198b0ce39a548aa1ecbd224d31
jaelomin <rainywalker@gmail.com>
2018-03-11 22:09:28 +0900

    webpack prod 수정

--- config/index.js
+++ config/index.js
@@ -60,7 +60,7 @@ module.exports = {
 
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: 'nosources-source-map',
+    // devtool: '#cheap-module-source-map',
       // cheap-module-source-map
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
